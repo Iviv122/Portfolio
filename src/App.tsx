bedf8084376d8d99c341bb12190ef9c8c7d2244d
Ivan <ivan@MacBook-Air-Ivan.local>
2026-05-05 10:20:20 +0200

    gh-pages and cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import AboutMe from "./components/aboutme"
+import Contact from "./components/contaxt"
 import PastExpirience from "./components/past_expirience"
 import Projects from "./components/projects"
 import Title from "./components/title"
 
 function App() {
   return (
-    <div className="flex flex-col items-center gap-3">
-      <br/>
-      <Title />
-      <br/>
-      <AboutMe/>
-      <br/>
-      <PastExpirience/>
-      <Projects/>
+    <div className="flex justify-center w-screen h-screen">
+      <div className="flex flex-col items-center gap-3 p-4 max-w-3xl">
+        <Title />
+        <Contact />
+        <br />
+        <AboutMe />
+        <br />
+        <PastExpirience />
+        <Projects />
+      </div>
     </div>
+
   )
 }
 
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,23 +1,47 @@
-
 export default function Projects() {
-
     const cards = [
         {
-            "title": "Test",
-            "description": "TEst s",
-            "link": "a.com"
+            title: "Zentube",
+            description: "Firefox extension to clean up your youtube page",
+            link: "https://github.com/Iviv122/zentube"
         },
-    ]
+        {
+            title: "Console3D",
+            description: "3D in your console, written in C++",
+            link: "https://github.com/Iviv122/Console3D"
+        },
+        {
+            title: "Cruster",
+            description: "Simple web-server written in rust",
+            link: "https://github.com/Iviv122/Cruster"
+        },
+        {
+            title: "Julia-set",
+            description: "Fractal visualization made in C++ with GLFW",
+            link: "https://github.com/Iviv122/Julia-set"
+        },
+        {
+            title: "Loan",
+            description: "Inifinte game made in Godot with GDscript",
+            link: "https://iviv122.itch.io/loan"
+        },
+        {
+            title: "Pasjans",
+            description: "Console implementation of Solitaire written in C#",
+            link: "https://github.com/Iviv122/Pasjans"
+        },
+    ];
 
     return (
         <div id="projects" className="flex flex-col gap-5">
-            <p className="text-center text-lg">Personal projects</p>
-            {
-                cards.map((el, i) =>
-                    <div key={i} className="space-y-1">
+            <p className="text-center text-lg">Highlighted personal projects</p>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                {cards.map((el, i) => (
+                    <div key={i}>
                         <h3 className="font-semibold">{el.title}</h3>
 
-                        <div className="text-sm text-gray-700">
+                        <div>
                             {el.description}
                         </div>
 
@@ -32,8 +56,8 @@ export default function Projects() {
                             </a>
                         )}
                     </div>
-                )
-            }
+                ))}
+            </div>
         </div>
     );
 }
